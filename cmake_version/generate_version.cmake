b05abbd6a1126e837aeaf5dcebfc0b953cee7148
Alexey Yablonsky <leahteslo@gmail.com>
2024-11-27 21:16:30 +0700

    feat: Добавил корневой проект
    - Добавлен файл сборки
    - Подключена библиотека логирования
    - Доавлена возможность автоматического версиорнирования

---
+++ cmake_version/generate_version.cmake
@@ -0,0 +1,20 @@
+﻿get_filename_component(SRC_DIR ${SRC} DIRECTORY)
+
+find_package(Git)
+
+if(((NOT DEFINED TEST_VERSION) OR (NOT TEST_VERSION)) AND GIT_EXECUTABLE)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} describe --abbrev=4 --tags --dirty --match "v*"
+        WORKING_DIRECTORY ${SRC_DIR}
+        OUTPUT_VARIABLE TEST_VERSION
+        RESULT_VARIABLE GIT_DESCRIBE_ERROR_CODE
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+endif()
+
+if((NOT DEFINED TEST_VERSION) OR (NOT TEST_VERSION) OR GIT_DESCRIBE_ERROR_CODE)
+    set(TEST_VERSION "v0.0.0-unknown")
+    message(WARNING "Failed to get version description from repository, use default instead.")
+endif()
+
+configure_file(${SRC} ${DST} @ONLY)
---
+++ main.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#define LOG_LVL LOG_LVL_VERBOSE
+#include "log.h"
+#include <version.h>
+
+
+int main(void)
+{
+    log_i(BUILD_SYSTEMS_VERSION);
+
+    return 0;
+}
